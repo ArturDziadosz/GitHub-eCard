f6b5cad8ab8e8498cb146e1e983fed6aef147768
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-02-26 12:01:48 +0100

    Added repos AJAX request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // waiting for the DOM to be loaded
 window.addEventListener("DOMContentLoaded", e => {
-  // AJAX request
+  // main AJAX request
   const fetchData = () => {
     // reading the input value
     const username = document.getElementById("username").value;
@@ -19,8 +19,10 @@ window.addEventListener("DOMContentLoaded", e => {
           if (request.status === 200) {
             // parsing data from JSON to JS
             const data = JSON.parse(request.responseText);
+            // calling repos AJAX request
+            reposFetchData(data.login);
             // printing data in result box
-            result.innerHTML = `<h2 class="box__resume__title">GitHub Résumé</h2><div class="box__resume__avatar"><img src="${data.avatar_url}"></div><div class="box__resume__result"><div class="result__box"><p>Username:</p><ul><li>${data.name}</li></ul></div><div class="result__box"><p>Website:</p><ul><li><a href="${data.html_url}" target="_blank">${data.html_url}</a></li></ul></div><div class="result__box"><p>Repositories:</p><ul><li>${data.name}</li></ul></div><div class="result__box"><p>Languages:</p><ul><li>${data.name}</li></ul></div></div>`;
+            result.innerHTML = `<h2 class="box__resume__title">GitHub Résumé</h2><div class="box__resume__avatar"><img src="${data.avatar_url}"></div><div class="box__resume__result"><div class="result__box"><p>Username:</p><ul><li>${data.name}</li></ul></div><div class="result__box"><p>Website:</p><ul><li><a href="${data.html_url}" target="_blank">${data.html_url}</a></li></ul></div><div class="result__box"><p>Repositories:</p><ul id="reposList"></ul></div><div class="result__box"><p>Languages:</p><ul id="languagesList"></ul></div></div>`;
           }
             // 404 user doesn't exist
             else if (request.status === 404) {
@@ -36,6 +38,47 @@ window.addEventListener("DOMContentLoaded", e => {
       request.send();
     }
   };
+
+  // repos AJAX request
+  const reposFetchData = (username) => {
+      // initialize a request
+      const request = new XMLHttpRequest();
+      request.open('GET', 'https://api.github.com/users/' + username + `/repos`);
+      // fire a main callback when readyState property of the request changes
+      request.onreadystatechange = () => {
+        // checking if fetch operation is complete
+        if (request.readyState === 4) {
+          // checking the successful response form server
+          if (request.status === 200) {
+            // parsing data from JSON to JS
+            const repos = JSON.parse(request.responseText);
+            // searching for created lists
+            const reposList = document.getElementById("reposList");
+            const languagesList = document.getElementById("languagesList");
+            // iterating whole repos array
+            repos.forEach(repo => {
+              // creating HTML element li
+              let li = document.createElement("li");
+              // adding content to the element li
+              li.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a><p>${repo.description}</p>`;
+              // appending each li element to repos ul
+              reposList.append(li);
+
+              //
+              li.innerHTML = `<p>${repo.language}</p>`;
+              //
+              languagesList.append(li);
+
+            });
+          }
+          else {
+            result.innerHTML = `<p class="userError">Ooops! Something goes wrong! Go to console.</p>`
+          }
+        }
+      };
+      // sending the request to the server
+      request.send();
+    };
 
   // variables
   const result = document.getElementById("result");
